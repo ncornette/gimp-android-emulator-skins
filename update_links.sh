aea27595d36090c2ee18e8d16379f6539678638e
Nicolas Cornette <nicolas.cornette@gmail.com>
2016-04-18 16:55:41 +0200

    Fix shell scripts for MACOSX

--- update_links.sh
+++ update_links.sh
@@ -3,30 +3,31 @@
 SDK_DIR=`echo $PATH | grep -i --color=never -o "[^:]*android[^:]*sdk[^:]*/tools[^:]*"`
 OIFS=$IFS
 IFS=$'\n'
-for d in $SDK_DIR; do
+for d in "${SDK_DIR}"; do
     SDK_DIR=`dirname $d`
 done
 
 # Check SDK_DIR
-if [ ! -d $SKIN_DIR]; then
+if [[ ! -d "${SDK_DIR}" ]]; then
     echo "Error : Cannot find Android SDK directory in PATH."
-    return 1
+    exit 1
 else
     echo "Using Android SDK directory: \"$SDK_DIR\""
 fi
 
 # Update links
-for android_skin_dir in $(find $SDK_DIR/platforms/android-* -type d -name skins); do
+for android_skin_dir in $(find ${SDK_DIR}/platforms/android-* -type d -name skins); do
     # Remove links
-    for eskin_link in $(find $android_skin_dir -type l -iname "ESKIN_*" ); do
-        echo "remove link: $eskin_link"
-        rm "$eskin_link"
+    for eskin_link in $(find ${android_skin_dir} -type l -iname "ESKIN_*" ); do
+        echo "remove link: ${eskin_link}"
+        rm "${eskin_link}"
     done
     
     # Create links
-    for eskin_dir in $(find ./ -type d -iname "ESKIN_*" ); do
-        target=$(readlink -f "$eskin_dir")
-        link=$(readlink -f "$android_skin_dir/$eskin_dir")
+    for eskin_dir in $(find . -type d -iname "ESKIN_*" ); do
+        skindir="$(basename ${eskin_dir})"
+        target="$PWD/$skindir"
+        link="${android_skin_dir}/$skindir"
         echo "create link: $target  ->  $link"
         ln -s "$target" "$link"
     done
